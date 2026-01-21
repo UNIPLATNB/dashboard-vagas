dd57352dbfd3ef80fc0fce81608b9b0484c8fbbf
UNIPLATNB <grp.nband@gmail.com>
2026-01-21 15:53:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,16 @@ const ordemSeries = [
   "2º período",
   "1º ano",
   "2º ano",
-  "3º ano"
+  "3º ano",
+  "4º ano",
+  "5º ano",
+  "6º ano",
+  "7º ano",
+  "8º ano",
+  "9º ano",
+  "1ª série",
+  "2ª série",
+  "3ª série"
 ];
 
 let dados = [];
