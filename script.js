9047b12318a3daed5ff5fcb61cbc3464956816e5
UNIPLATNB <grp.nband@gmail.com>
2026-01-21 14:06:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const planilhaId = "12Ou7DzGLBmYIxUDJqvgRnVUejheSMTSoD0dtkC_50BE";
 
 const seriesMap = [
-  { serie: 3, vaga: 4 },
-  { serie: 6, vaga: 7 },
-  { serie: 9, vaga: 10 },
-  { serie: 12, vaga: 13 },
-  { serie: 15, vaga: 16 },
-  { serie: 18, vaga: 19 },
-  { serie: 21, vaga: 22 }
+  { serie: 3, vaga: 4 },    // D / E
+  { serie: 6, vaga: 7 },    // G / H
+  { serie: 9, vaga: 10 },   // J / K
+  { serie: 12, vaga: 13 },  // M / N
+  { serie: 15, vaga: 16 },  // P / Q
+  { serie: 18, vaga: 19 },  // S / T
+  { serie: 21, vaga: 22 },  // V / W
+  { serie: 24, vaga: 25 },  // Y / Z
+  { serie: 27, vaga: 28 },  // AB / AC
+  { serie: 30, vaga: 31 },  // AE / AF
+  { serie: 33, vaga: 34 },  // AH / AI
+  { serie: 36, vaga: 37 }   // AK / AL
 ];
 
 function carregarRegiao(aba) {
